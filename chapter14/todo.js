fe3183b3118433520837fa36dc83a1fa29829b63
sethmchris <sethmchris@gmail.com>
2017-04-19 14:15:19 -0600

    Add files via upload

--- chapter14/todo.js
+++ chapter14/todo.js
@@ -1,3 +1,24 @@
 $(document).ready(function(e) {
+	$('#add-todo').button({
+		icons: {
+			icons: {
+				primary: "ui-icon-circle-plus"
+			}
+		}
+	}).click(function() {
+		$('#new-todo').dialog('open');
+	}); // end button
 
+	$('#new-todo').dialog({
+		modal: true, // require user to close box
+		autoOpen: false,
+		buttons : {
+			"Add task" : function () {
+
+			},
+			"Cancel": function () {
+				$(this).dialog('close');
+			}
+		}
+	}); // end dialog
 }); // end ready
